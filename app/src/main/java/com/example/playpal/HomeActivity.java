570bf54b84096c80350fcff7699eb43d1675b55d
PradyZaddy <138837542+PradyZaddy@users.noreply.github.com>
2025-03-22 16:12:21 -0700

    test - commit

--- app/src/main/java/com/example/playpal/HomeActivity.java
+++ app/src/main/java/com/example/playpal/HomeActivity.java
@@ -4,27 +4,23 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-/**
- * This activity is a base activity for the four main app screens:
- * Posts feed, Map, Schedule, Profile. This class manages transition
- * between the screens, which are displayed using fragments. The transition
- * is done by clicking on the respective icons in the bottom navigation bar.
- */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity
+{
+    PostFragment postFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        postFragment = new PostFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frameLayout, postFragment);
+        ft.commit();
     }
 }
